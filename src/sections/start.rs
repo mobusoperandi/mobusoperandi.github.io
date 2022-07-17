6126343b4f1583ebe65dae27426f39699d8696ce
Vimal Patel <mailtovimal@gmail.com>
2022-07-17 08:43:52 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/sections/start.rs

--- src/sections/start.rs
+++ src/sections/start.rs
@@ -3,24 +3,11 @@ use maud::html;
 pub fn section() -> Section {
     Section::new(
         "start".into(),
-        "".into(),
+        "flex flex-col justify-items-center".into(),
         None,
         html! {
             h1 class="[word-spacing:100vw] text-center text-[min(7vw,15vh)] [line-height:1]" { "STUDY RUST ONLINE IN MOB-PROGRAMMING FORMAT" }
+            a class="border border-r-4 p-4 text-center" { "SEE EXISTING MOBS" }
         },
     )
-
-    // "main".into(),
-    // "".into(),
-    // None,
-    // html! {
-    //     h1 { "An online community for the regular study of software development in mob programming format" }
-    //     ul {
-    //         li { a href="#in_the_media" { "in_the_media" } }
-    //         li { a href="#mobs_calendar" { "mobs_calendar" } }
-    //         li { a href="#join" { "join" } }
-    //         li { a href="#mightyiam" { "mightyiam" } }
-    //         li { a href="#why_mob" { "why_mob" } }
-    //     }
-    // },
 }
