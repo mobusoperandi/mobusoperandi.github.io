9a7ca9c60b0b1fc51b238229471737cb05294245
Roland Fredenhagen <dev@modprog.de>
2022-07-17 09:57:48 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/sections/start.rs

--- src/sections/start.rs
+++ src/sections/start.rs
@@ -2,12 +2,16 @@ use super::Section;
 use maud::html;
 pub fn section() -> Section {
     Section::new(
-        "start".into(),
-        "flex flex-col justify-items-center".into(),
+        "main".into(),
+        "flex flex-col items-center justify-center".into(),
         None,
         html! {
-            h1 class="[word-spacing:100vw] text-center text-[min(7vw,15vh)] [line-height:1]" { "STUDY RUST ONLINE IN MOB-PROGRAMMING FORMAT" }
-            a class="border border-r-4 p-4 text-center" { "SEE EXISTING MOBS" }
+            h1 class="[word-spacing:100vw] text-center text-[min(7vw,11vh)] [line-height:1.1]" { 
+                p {"STUDY RUST ONLINE"}
+                p {"IN"}
+                p {"MOB-PROGRAMMING FORMAT"}
+            }
+            a href="#mobs_calendar" class="border-4 rounded-xl p-4 text-center border-current text-xl mt-10" { "SEE EXISTING MOBS" }
         },
     )
 }
